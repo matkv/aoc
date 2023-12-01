d477c721b2119116d9283d9fb6aa968f88eb0fac
matkv <mail@matkv.dev>
2023-12-01 17:23:17 +0100

    day one solve part 1

--- day1/trebuchet.go
+++ day1/trebuchet.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func readFile(filename string) (string, error) {
@@ -14,8 +17,28 @@ func readFile(filename string) (string, error) {
 }
 
 func solvePartOne(input string) int {
-	// Implement your solution for Part One here
-	return 0 // Placeholder return value
+
+	re := regexp.MustCompile("[0-9]")
+	var lines = strings.Split(input, "\n")
+
+	var sum int
+
+	for _, line := range lines {
+		var numbersInString = re.FindAllString(line, -1)
+
+		if len(numbersInString) > 0 {
+
+			var firstDigit, _ = strconv.ParseInt(numbersInString[0], 10, 64)
+			var lastDigit, _ = strconv.ParseInt(numbersInString[len(numbersInString)-1], 10, 64)
+
+			var generatedNumberString = strconv.FormatInt(firstDigit, 10) + strconv.FormatInt(lastDigit, 10)
+			var generatedNumber, _ = strconv.Atoi(generatedNumberString)
+
+			sum = sum + generatedNumber
+		}
+	}
+
+	return sum // Placeholder return value
 }
 
 func solvePartTwo(input string) int {
