a2f7369f026a7502a97c0e799006fe35eb9b0501
matkv <mail@matkv.dev>
2023-12-01 18:51:25 +0100

    no idea what I'm doing anymore

--- day1/trebuchet.go
+++ day1/trebuchet.go
@@ -38,16 +38,60 @@ func solvePartOne(input string) int {
 		}
 	}
 
-	return sum // Placeholder return value
+	return sum
 }
 
 func solvePartTwo(input string) int {
-	// Implement your solution for Part Two here
-	return 0 // Placeholder return value
+	var lines = strings.Split(input, "\n")
+
+	var numbersList []int
+
+	for _, str := range lines {
+		var currentNumber int
+
+		for _, char := range str {
+			// Extract consecutive letters to form a word
+			if char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z' {
+				currentNumberStr := string(char)
+				// Check if spelled-out number word is found
+				if num, ok := spelledOutNumberToDigit(currentNumberStr); ok {
+					currentNumber = currentNumber*10 + num // Accumulate the digit
+				}
+			} else {
+				// Reset the currentNumber if non-letter character is encountered
+				currentNumber = 0
+			}
+
+			if currentNumber != 0 {
+				numbersList = append(numbersList, currentNumber)
+			}
+		}
+	}
+	return 0
+}
+
+func spelledOutNumberToDigit(word string) (int, bool) {
+	digitWords := map[string]int{
+		"zero":  0,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
+	if digit, lookupSuccessful := digitWords[strings.ToLower(word)]; lookupSuccessful {
+		return digit, true
+	}
+	return -1, false
 }
 
 func main() {
-	input, err := readFile("input.txt") // Replace "input.txt" with your input file
+	input, err := readFile("exampleinput2.txt") // Replace "input.txt" with your input file
 	if err != nil {
 		fmt.Println("Error reading input file:", err)
 		return
