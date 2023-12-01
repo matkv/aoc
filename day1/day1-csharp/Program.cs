658e3463ec2f74dd2012c9f5bc67bd92d292bca0
matkv <mail@matkv.dev>
2023-12-01 22:04:25 +0100

    finished task 2 in c#, misread the task initially, gonna kill myself :)

--- day1/day1-csharp/Program.cs
+++ day1/day1-csharp/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.SymbolStore;
-using System.Runtime.CompilerServices;
+﻿using System.Text.RegularExpressions;
 
 internal class Program
 {
@@ -24,16 +23,71 @@ internal class Program
                     numbersFoundInLine.Add(number);
             }
 
-            var constructedNumber = $"{numbersFoundInLine.First()}" + $"{numbersFoundInLine.Last()}";
-            result += Int32.Parse(constructedNumber);
+            if (numbersFoundInLine.Count > 0)
+                result = AddFirstAndLastDigitInLine(result, numbersFoundInLine);
         }
 
         return result;
     }
 
+    private static int AddFirstAndLastDigitInLine(int result, List<int> numbersFoundInLine)
+    {
+        var constructedNumber = $"{numbersFoundInLine.First()}" + $"{numbersFoundInLine.Last()}";
+        result += Int32.Parse(constructedNumber);
+        return result;
+    }
+
     private static int SolveTaskPart2(IEnumerable<string> input)
     {
-        return 0;
+        int result = 0;
+        Dictionary<string, int> stringToNumber = new Dictionary<string, int>
+        {
+            { "zero", 0 },
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 },
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 },
+            { "0", 0 },
+            { "1", 1 },
+            { "2", 2 },
+            { "3", 3 },
+            { "4", 4 },
+            { "5", 5 },
+            { "6", 6 },
+            { "7", 7 },
+            { "8", 8 },
+            { "9", 9 }
+        };
+
+
+        foreach (var line in input.ToList())
+        {
+            var numbersFoundInLine = new List<int>();
+
+            var allPossibleSubstrings =
+            from i in Enumerable.Range(0, line.Length)
+            from j in Enumerable.Range(0, line.Length - i + 1)
+            where j >= 1
+            select line.Substring(i, j);
+
+            foreach (var substring in allPossibleSubstrings)
+            {
+                if (stringToNumber.ContainsKey(substring))
+                {
+                    numbersFoundInLine.Add(stringToNumber[substring]);
+                }
+            }
+
+            if (numbersFoundInLine.Count > 0)
+                result = AddFirstAndLastDigitInLine(result, numbersFoundInLine);
+        }
+
+        return result;
     }
 
 
