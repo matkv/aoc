86288aef080ef4410cb47930a50c419f3469aa52
matkv <mail@matkv.dev>
2023-12-01 20:20:12 +0100

    solve day 1 part 1 in C#

--- day1/day1-csharp/Program.cs
+++ day1/day1-csharp/Program.cs
@@ -5,6 +5,37 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
+        var input = File.ReadLines("input.txt");
+        var answer1 = SolveTaskPart1(input);
+        var answer2 = SolveTaskPart2(input);
+        Console.WriteLine($"Answer 1: {answer1}, Answer 2: {answer2}");
     }
+
+    private static int SolveTaskPart1(IEnumerable<string> input)
+    {
+        int result = 0;
+        foreach (var line in input.ToList())
+        {
+            List<int> numbersFoundInLine = new List<int>();
+
+            foreach (var character in line)
+            {
+                if (Int32.TryParse(character.ToString(), out int number))
+                    numbersFoundInLine.Add(number);
+            }
+
+            var constructedNumber = $"{numbersFoundInLine.First()}" + $"{numbersFoundInLine.Last()}";
+            result += Int32.Parse(constructedNumber);
+        }
+
+        return result;
+    }
+
+    private static int SolveTaskPart2(IEnumerable<string> input)
+    {
+        return 0;
+    }
+
+
+
 }
